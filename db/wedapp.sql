34e83ba541fa2d20c209f0f656f6e9a97bfba7d7
tayyab9ahmed <tayyabahmed4u@hotmail.com>
2018-06-14 14:00:50 +0500

    Add files via upload

---
+++ db/wedapp.sql
@@ -0,0 +1,161 @@
+/*
+SQLyog Professional v12.09 (64 bit)
+MySQL - 5.5.32 : Database - wedapp
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`wedapp` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `wedapp`;
+
+/*Table structure for table `events` */
+
+DROP TABLE IF EXISTS `events`;
+
+CREATE TABLE `events` (
+  `Event_id` int(11) NOT NULL AUTO_INCREMENT,
+  `Event_title` varchar(128) DEFAULT NULL,
+  `Event_type` int(11) DEFAULT NULL,
+  `Event_start_date` datetime DEFAULT NULL,
+  `Event_end_date` datetime DEFAULT NULL,
+  `Event_expected_guest` varchar(50) DEFAULT NULL,
+  `CreatedBy` int(11) DEFAULT NULL,
+  `Created_datetime` datetime DEFAULT NULL,
+  `Modified_by` int(11) DEFAULT NULL,
+  `Modified_date` datetime DEFAULT NULL,
+  `Is_Active` bit(1) DEFAULT b'1',
+  `User_id` int(11) DEFAULT NULL,
+  `Vendor_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Event_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `events` */
+
+/*Table structure for table `service` */
+
+DROP TABLE IF EXISTS `service`;
+
+CREATE TABLE `service` (
+  `Service_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `Service_title` varchar(50) DEFAULT NULL,
+  `Vendor_type_id` int(11) DEFAULT NULL,
+  KEY `Service_id` (`Service_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=latin1;
+
+/*Data for the table `service` */
+
+insert  into `service`(`Service_id`,`Service_title`,`Vendor_type_id`) values (1,'People Accomodation\r\n',1),(2,'Rental fees per person',1),(3,'Hall or outdoor places\r',1),(4,'Parking space\r\n',1),(5,'Restrooms',1),(6,'Room for bride \r\n',1),(7,'Tenting Service',1),(8,'Cancellation Policy',1),(9,'First Aid Box',1),(10,'Food Authority license. \r\n',4),(11,'Food Quality \r\n',4),(12,'Ratio of servers to guest\r\n',4),(13,'Cancellation Policy',4),(14,'Type of Music',3),(15,'Any smoke or lighting effects\r\n',3),(16,'hours included in package \r\n',3),(17,'Any backup plan in case of equipement malfunction.',3),(18,'Cancellation Policy',3),(19,'Work on my location or yours ?\r\n',7),(20,'Cancellation Policy.\r\n',7),(21,'Guest with me',7),(22,'First Aid Box.',7),(23,'Charge per milage or hour.',9),(24,'Cancellation policy.\r\n',9),(25,'Auto Insurance\r\n',9),(26,'Driver avalable\r\n',9),(27,'First Aid Box.\r\n',9),(28,'time to setup.',2),(29,'Any lighting effects.\r\n',2),(30,'hours included in package. \r\n',2),(31,'Any backup plan in case of equipement malfunction.',2),(32,'Cancellation Policy.\r\n',2),(33,'Fresh flowers.\r\n',2),(34,'Camera Quality',5),(35,'Shoot Or Film.',5),(36,'Videographer ?',5),(37,'Photography style (traditional, photojournalistic,',5),(38,'Any backup plan in case of equipement malfunction.',5),(39,'Deliver of photos with album in no of days.',5),(40,'Copyright issue.\r\n',5);
+
+/*Table structure for table `user_type` */
+
+DROP TABLE IF EXISTS `user_type`;
+
+CREATE TABLE `user_type` (
+  `User_type_id` int(11) NOT NULL AUTO_INCREMENT,
+  `USer_type_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`User_type_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `user_type` */
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `User_id` int(11) NOT NULL AUTO_INCREMENT,
+  `User_name` varchar(255) DEFAULT NULL,
+  `User_email` varchar(255) DEFAULT NULL,
+  `User_phone_no` int(11) DEFAULT NULL,
+  `User_password` varchar(128) DEFAULT NULL,
+  `User_type_id` int(11) DEFAULT NULL,
+  `CreatedBy` int(11) DEFAULT NULL,
+  `CreatedOn` datetime DEFAULT NULL,
+  `ModifiedBy` int(11) DEFAULT NULL,
+  `ModifiedOn` datetime DEFAULT NULL,
+  `isDeleted` int(1) DEFAULT NULL,
+  PRIMARY KEY (`User_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `users` */
+
+/*Table structure for table `vendor_pictures` */
+
+DROP TABLE IF EXISTS `vendor_pictures`;
+
+CREATE TABLE `vendor_pictures` (
+  `Vendor_picture_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `Vendor_id` int(11) DEFAULT NULL,
+  `Vendor_picture_path` varchar(50) DEFAULT NULL,
+  KEY `Vendor_picture_id` (`Vendor_picture_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `vendor_pictures` */
+
+/*Table structure for table `vendor_service` */
+
+DROP TABLE IF EXISTS `vendor_service`;
+
+CREATE TABLE `vendor_service` (
+  `Vendor_service_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `Vendor_id` int(11) DEFAULT NULL,
+  `Service_id` int(11) DEFAULT NULL,
+  `creation_date` datetime DEFAULT NULL,
+  `isdeleted` bit(1) DEFAULT b'0',
+  KEY `Vendor_service_id` (`Vendor_service_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `vendor_service` */
+
+/*Table structure for table `vendor_type` */
+
+DROP TABLE IF EXISTS `vendor_type`;
+
+CREATE TABLE `vendor_type` (
+  `Vendor_type_id` int(11) NOT NULL AUTO_INCREMENT,
+  `Vendor_type_name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`Vendor_type_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+
+/*Data for the table `vendor_type` */
+
+insert  into `vendor_type`(`Vendor_type_id`,`Vendor_type_name`) values (1,'Banquet Hall'),(2,'Decorators'),(3,'Dj'),(4,'Catering'),(5,'Photographer'),(6,'Bakeries'),(7,'Bridal Saloon'),(8,'Invitation'),(9,'Limousine'),(10,'Florist');
+
+/*Table structure for table `vendors` */
+
+DROP TABLE IF EXISTS `vendors`;
+
+CREATE TABLE `vendors` (
+  `Vendor_id` int(11) NOT NULL AUTO_INCREMENT,
+  `Vendor_name` varchar(255) DEFAULT NULL,
+  `Vendor_type` int(11) DEFAULT NULL,
+  `Vendor_contact_no` int(11) DEFAULT NULL,
+  `Vendor_email_address` varchar(255) DEFAULT NULL,
+  `Vendor_address` varchar(255) DEFAULT NULL,
+  `Vendor_owner_name` varchar(255) DEFAULT NULL,
+  `Vendor_owner_contact_no` int(11) DEFAULT NULL,
+  `CreatedBy` int(11) DEFAULT NULL,
+  `CreatedOn` datetime DEFAULT NULL,
+  `ModifiedBy` int(11) DEFAULT NULL,
+  `ModifiedOn` datetime DEFAULT NULL,
+  `isDeleted` int(1) DEFAULT '0',
+  `City` varchar(50) DEFAULT NULL,
+  KEY `Vendor_id` (`Vendor_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+
+/*Data for the table `vendors` */
+
+insert  into `vendors`(`Vendor_id`,`Vendor_name`,`Vendor_type`,`Vendor_contact_no`,`Vendor_email_address`,`Vendor_address`,`Vendor_owner_name`,`Vendor_owner_contact_no`,`CreatedBy`,`CreatedOn`,`ModifiedBy`,`ModifiedOn`,`isDeleted`,`City`) values (1,'Gourmet Central Marquee',1,2147483647,NULL,'30 K, Mini Market, Gulberg II, Pakistan, Sir Syed Rd, Lahore','Gourmet',2147483647,NULL,NULL,NULL,NULL,0,'Lahore'),(2,'Eastern Marquee',1,2147483647,NULL,'210 Wafaqi Colony, Main Upper Canal Bank Road, Lahore - Pakistan','Eastern Marquee',2147483647,NULL,NULL,NULL,NULL,0,'Lahore'),(3,'Creative Zafar Marquee',1,2147483647,NULL,'Falcon Complex Gulberg III. Lahore, Pakistan','Zafar',2147483647,NULL,NULL,NULL,NULL,0,'Lahore'),(4,'Pakistan Rangers Marriage & Banquet Hall',1,2147483647,NULL,'Zarar Shaheed Road, Near Joray Bridge, Lahore Cantt, Lahore','Rangers',2147483647,NULL,NULL,NULL,NULL,0,'Lahore'),(5,'Taj Mahal Banquet Hall',1,966,NULL,'Ferozepur Road, Lahore',NULL,966,NULL,NULL,NULL,NULL,0,'Lahore'),(6,'Mughal-e-Azam Banquet Complex',1,321,NULL,'Usmani Rd, Lahore ',NULL,321,NULL,NULL,NULL,NULL,0,'Lahore '),(7,'Maharaja Palace',1,2147483647,NULL,'38 Maulana Shaukat Ali Road, Lahore 54770',NULL,2147483647,NULL,NULL,NULL,NULL,0,'Lahore '),(8,'Lahore Castle Banquet Hall',1,2147483647,NULL,'Block D1, Nespak Housing Society, Sutlej Ave, Lahore',NULL,2147483647,NULL,NULL,NULL,NULL,0,'Lahore '),(9,'Blessing Banquet Hall',1,2147483647,NULL,'9 civic center, Johar Town, Lahore',NULL,NULL,NULL,NULL,NULL,NULL,0,'Lahore '),(10,'Four Seasons Wedding & Banquet Halls',NULL,2147483647,NULL,'13 Raiwind Rd, Lahore',NULL,NULL,NULL,NULL,NULL,NULL,0,'Lahore '),(12,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,NULL);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
---
+++ js/script.js
@@ -0,0 +1,499 @@
+
+jQuery(function(){
+
+// start timecounter
+  var clock;
+  var ck_string = [];
+  clock = $('#timecounter').FlipClock({
+        clockFace: 'DailyCounter',
+        autoStart: false,
+        callbacks: {
+          stop: function() {
+            $('.message').html('The clock has stopped!')
+          }
+        }
+    });
+
+    clock.setTime(220880);
+    clock.setCountdown(true);
+    clock.start();
+/* end time counter
+  $('#phoneNumber').on('change', function () {
+    debugger;
+      var phone_num_regex = /^((\+92)|(0092))-{0,1}\d{3}-{0,1}\d{7}$|^\d{11}$|^\d{4}-\d{7}$/i;
+      if (phone_num_regex.test($(this).val())) {
+        $('#message_phone').html('Valid Pakistan Phone Number').css('color', 'green');
+      }
+      else {
+        $('#message_phone').html('Invalid Pakistan Phone Number').css('color', 'red');
+      }
+  });
+
+  $('#confirm_password').on('keyup', function () {
+      if ($(this).val() == $('#password').val()) {
+          $('#message_pass').html('Matching').css('color', 'green');
+      } else $('#message_pass').html('Not Matching').css('color', 'red');
+  });
+
+    //Singup Validation
+  /*  $('#SingupForm')
+        .formValidator({
+            FeedbackIcons: {
+                valid: 'glyphicon glyphicon-ok',
+                invalid: 'glyphicon glyphicon-remove',
+                validating: 'glyphicon glyphicon-refresh'
+            },
+            fields: {
+                phoneNumber: {
+                    required : true,
+                    minlength:11,
+                    validators: {
+                        phone: {
+                            message: 'The value is not valid pakistan phone number'
+                        }
+                    }
+                }
+            }
+        })
+        // Revalidate phone number when changing the country
+        .on('change' , function(e) {
+            $('#SingupForm').formValidation('revalidateField', 'phoneNumber');
+        });
+
+*/
+
+
+$(".switch").click(function() {
+  debugger;
+  $.ajax({ url: "../Vendor/get_services_by_vendor_type", data:"vendor_type_id="+$(this).val(), success: function(result){
+    debugger;
+    $("#panel-body-service").html(result);
+  }});
+});
+
+//login form validation and submittion
+$('.login').click(function(){
+debugger;
+
+$('#user-login').bootstrapValidator({
+  trigger: 'blur',
+  fields: {
+      User_email: {
+          validators: {
+              notEmpty: {
+                  message: 'The email is required'
+              },
+              emailAddress: {
+                  message: 'User email is not a valid email address'
+              }
+          }
+      },
+      User_password: {
+              validators: {
+                  notEmpty: {
+                      message: 'The password is required and can\'t be empty'
+                  }
+              }
+          }
+  }
+});
+if($('#user-login').bootstrapValidator('validate').has('.has-error').length > 0){
+  return false;
+}
+else {
+  debugger;
+  $.ajax({type: "POST", url: "dologin", data:$("#user-login").serialize(), success: function(result){
+      if(result == "true")
+      {
+        window.location.href = 'http://localhost:8080/Wedishedi/Welcome/dashboard';
+      }
+      else {
+        window.location.href = 'http://localhost:8080/Wedishedi/Welcome/login/?result=false';
+      }
+
+	  }});
+}
+});
+// signup form validation and submittion
+$('.signup').click(function(){
+debugger;
+$('#user_signup').bootstrapValidator({
+  trigger: 'blur',
+  fields: {
+      User_fname: {
+          validators: {
+              notEmpty: {
+                  message: 'First name is required'
+              },
+              regexp: {
+                  regexp: /^[a-zA-Z ]+$/,
+                  message: 'First name cannot have numbers or symbols'
+              }
+          }
+      },
+      User_lname: {
+          validators: {
+              notEmpty: {
+                  message: 'Last name is required'
+              },
+              regexp: {
+                  regexp: /^[a-zA-Z ]+$/,
+                  message: 'Last name cannot have numbers or symbols'
+              }
+          }
+      },
+      User_email: {
+          validators: {
+              notEmpty: {
+                  message: 'The email is required'
+              },
+              emailAddress: {
+                  message: 'User email is not a valid email address'
+              }
+          }
+      },
+      User_phone_no: {
+          validators: {
+              notEmpty: {
+                  message: 'User contact number is required'
+              },
+              regexp: {
+                  regexp: /^((\+92)|(0092))-{0,1}\d{3}-{0,1}\d{7}$|^\d{11}$|^\d{4}-\d{7}$/,
+                  message: 'The input is not a valid Pakistan phone number'
+              }
+          }
+      },
+      User_password: {
+              validators: {
+                  notEmpty: {
+                      message: 'The password is required and can\'t be empty'
+                  },
+                  identical: {
+                      field: 'confirm_password',
+                      message: 'The password and its confirm are not the same'
+                  }
+              }
+          }
+  }
+});
+
+$.ajax({type: "POST", url: "chk_email", async: false, data:{User_email:$("#User_email").val()}, success: function(result){
+    debugger;
+    if(result == "false")
+    {
+     $('#User_email').css({'border-color':'#a94442'});
+     $('#message_phone').html('Email already exists. Please Login with your existed account.').css({'color':'#a94442','font-size':'12px'});
+     return false;
+    }
+    else {
+      $('#User_email').css({'border-color':'#3c763d'});
+      $('#message_phone').html('');
+      return true;
+    }
+  }});
+if($('#user_signup').bootstrapValidator('validate').has('.has-error').length > 0 || $('#message_phone').html().length > 0){
+    return false;
+}
+else {
+  debugger;
+
+  $.ajax({type: "POST", url: "save", async: false, data:$("#user_signup").serialize(), success: function(result){
+       window.location.href = 'http://localhost:8080/Wedishedi/Welcome/login';
+	  }});
+}
+});
+
+
+    $("#service-slider").owlCarousel({
+        items : 3,
+        itemsCustom : false,
+        itemsDesktop : [1199,3],
+        itemsDesktopSmall : [980,2],
+        itemsTablet: [768,2],
+        autoPlay : true,
+        stopOnHover : true,
+        pagination : true,
+        paginationNumbers: false,
+    });
+
+    $("#testimonial-slider").owlCarousel({
+        items : 2,
+        itemsCustom : false,
+        itemsDesktop : [1199,2],
+        itemsDesktopSmall : [980,2],
+        itemsTablet: [768,2],
+        autoPlay : true,
+        stopOnHover : true,
+        pagination : true,
+        paginationNumbers: false,
+    });
+
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1200
+    });
+
+    $('.hamburger').click(function(){
+        $('.header-menu').fadeToggle(200);
+    });
+
+
+  /*  $('.datepicker').datepicker({
+       format:"dd-mm-yyyy",
+       ignoreReadonly: false
+        }).on('changeDate', function(e){
+        (e.viewMode=='days') ? $(this).datepicker('hide') : '';
+   });
+*/
+    $('.switch').switchable({
+        label_position: 'after'
+        ,click: function( ev, checked )
+            {
+              debugger;
+              $.each($("input[name='Vendor_type[]']:not(:checked)"), function(){
+                debugger;
+                $("#"+$(this).val()+"").remove();
+                var index = ck_string.indexOf($(this).val());
+                if( index > -1 )
+                {
+                  ck_string.splice(index, 1);
+                }
+              });
+
+            $.each($("input[name='Vendor_type[]']:checked"), function(){
+              debugger;
+              if(ck_string.indexOf($(this).val()) > -1 )
+              {
+
+              }
+              else {
+                ck_string.push($(this).val());
+                $.ajax({ url: "../Vendor/get_services_by_vendor_type", data:"vendor_type_id="+$(this).val(), success: function(result){
+                  debugger;
+                  $("#panel-body-service").append(result);
+                }});
+              }
+
+            });
+
+            }
+    });
+
+    $('.closebutton').click(function() {
+        var parent = $(this).closest('div');
+        var head = parent.prev('h3');
+        var next = parent.nextAll();
+        parent.add(head).add(next).fadeOut('slow',function(){$(this).remove();});
+    });
+
+
+    $('.next-2').click(function(){
+        debugger;
+        debugger;
+      	$('#vendor_add').bootstrapValidator({
+          trigger: 'blur',
+          fields: {
+              Vendor_name: {
+                  validators: {
+                      notEmpty: {
+                          message: 'Vendor name is required'
+                      },
+                      regexp: {
+                          regexp: /^[a-zA-Z ]+$/,
+                          message: 'Vendor name cannot have numbers or symbols'
+                      }
+                  }
+              },
+              Vendor_contact_no: {
+                  validators: {
+                      notEmpty: {
+                          message: 'Vendor contact number is required'
+                      },
+                      regexp: {
+                          regexp: /^((\+92)|(0092))-{0,1}\d{3}-{0,1}\d{7}$|^\d{11}$|^\d{4}-\d{7}$/,
+                          message: 'The input is not a valid Pakistan phone number'
+                      }
+                  }
+              },
+              Vendor_email_address: {
+                  validators: {
+                      notEmpty: {
+                          message: 'The email is required'
+                      },
+                      emailAddress: {
+                          message: 'Vendor email is not a valid email address'
+                      }
+                  }
+              },
+      				Vendor_description: {
+                  validators: {
+                      notEmpty: {
+                          message: 'Vendor Description is required'
+                      }
+                  }
+              },
+      				Vendor_address: {
+                  validators: {
+                      notEmpty: {
+                          message: 'Vendor Address is required'
+                      }
+                  }
+              },
+      				Vendor_owner_name: {
+                  validators: {
+                      notEmpty: {
+                          message: 'Owner name is required'
+                      },
+                      regexp: {
+                          regexp: /^[a-zA-Z ]+$/,
+                          message: 'Owner name cannot have numbers or symbols'
+                      }
+                  }
+              },
+              Vendor_owner_contact_no: {
+                  validators: {
+                      notEmpty: {
+                          message: 'Owner phone number is required'
+                      },
+                      regexp: {
+                          regexp: /^((\+92)|(0092))-{0,1}\d{3}-{0,1}\d{7}$|^\d{11}$|^\d{4}-\d{7}$/,
+                          message: 'The input is not a valid Pakistan phone number'
+                      }
+                  }
+              },
+      				city: {
+                  validators: {
+                      notEmpty: {
+                          message: 'City is required'
+                      }
+                  }
+              }
+          }
+      	});
+      	if($('#vendor_add').bootstrapValidator('validate').has('.has-error').length > 0){
+      		return false;
+      	}
+      else {
+        var file_name=$("#Vendor_picture").val();
+            var fileName = $("#Vendor_picture").val();
+            var fileExtension = fileName.substring(fileName.lastIndexOf('.') + 1);
+            var file_data=$("#Vendor_picture").prop("files")[0];
+            var form = $('#vendor_add').serialize();
+            var form_data= new FormData($('form')[0]);
+            form_data.append("file",file_data);
+        $.ajax({type: "POST", url: "../Vendor/save", data:form_data, cache : false,contentType: false,
+    processData: false,success: function(result){
+	    debugger;
+    			window.location.href = 'http://localhost:8080/Wedishedi/Welcome/dashboard';
+    	  },
+          error:function()
+        {
+          return false;
+        }});
+        //$('.step-one').fadeOut(200);
+        //$('.step-three').fadeIn(200);
+
+        //$('#step1').removeClass('active');
+        //$('#step2').addClass('active');
+      }
+
+    });
+
+    $('.next-3').click(function(){
+      debugger;
+      $('.input_validator').bootstrapValidator({
+        trigger: 'blur',
+        required:true,
+        message: 'City is required'
+        });
+        if($('#vendor_add').bootstrapValidator('validate').has('.has-error').length > 0){
+      		return false;
+      	}
+        $('.step-one').fadeOut(200);
+        $('.step-three').fadeIn(200);
+        $('.step-two').fadeOut(200);
+
+        $('#step1').removeClass('active');
+        $('#step3').addClass('active');
+        $('#step2').removeClass('active');
+    });
+
+    $('.prev-1').click(function(){
+        $('.step-two').fadeOut(200);
+        $('.step-one').fadeIn(200);
+        $('.step-three').fadeOut(200);
+
+        $('#step2').removeClass('active');
+        $('#step1').addClass('active');
+        $('#step3').removeClass('active');
+    });
+
+    $('.prev-2').click(function(){
+        $('.step-one').fadeIn(200);
+        $('.step-three').fadeOut(200);
+
+        $('#step1').addClass('active');
+        $('#step3').removeClass('active');
+    });
+
+});
+
+
+var $hamburger = $(".hamburger");
+    $hamburger.on("click", function(e) {
+    $hamburger.toggleClass("is-active");
+    // Do something else, like open/close menu
+});
+$(document).scroll(function() {
+  if($('.nav-search').offset() != null || $('.nav-search').offset() != undefined)
+  {
+  var fixmeTop = $('.nav-search').offset().top;
+    var currentScroll = $(window).scrollTop();
+    if (currentScroll >= fixmeTop && fixmeTop > 450) {
+        $('.nav-search').css({
+            position: 'fixed',
+            top: '0',
+            left: '0'
+        });
+    } else {
+        $('.nav-search').css({
+            position: 'static'
+        });
+    }
+  }
+});
+
+
+/*button hover
+===========================*/
+
+    var buttons7Click = Array.prototype.slice.call( document.querySelectorAll( '#btn-click button' ) ),
+    buttons9Click = Array.prototype.slice.call( document.querySelectorAll( 'button.btn-8g' ) ),
+    totalButtons7Click = buttons7Click.length,
+    totalButtons9Click = buttons9Click.length;
+
+    buttons7Click.forEach( function( el, i ) { el.addEventListener( 'click', activate, false ); } );
+    buttons9Click.forEach( function( el, i ) { el.addEventListener( 'click', activate, false ); } );
+    function activate() {
+    var self = this, activatedClass = 'btn-activated';
+
+    if( classie.has( this, 'btn-7h' ) ) {
+    // if it is the first of the two btn-7h then activatedClass = 'btn-error';
+        // if it is the second then activatedClass = 'btn-success'
+    activatedClass = buttons7Click.indexOf( this ) === totalButtons7Click-2 ? 'btn-error' : 'btn-success';
+                    }
+    else if( classie.has( this, 'btn-8g' ) ) {
+                    // if it is the first of the two btn-8g then activatedClass = 'btn-success3d';
+                    // if it is the second then activatedClass = 'btn-error3d'
+        activatedClass = buttons9Click.indexOf( this ) === totalButtons9Click-2 ? 'btn-success3d' : 'btn-error3d';
+    }
+
+    if( !classie.has( this, activatedClass ) ) {
+        classie.add( this, activatedClass );
+        setTimeout( function() { classie.remove( self, activatedClass ) }, 1000 );
+    }
+    }
+
+    document.querySelector( '.btn-7i' ).addEventListener( 'click', function() {
+        classie.add( document.querySelector( '#trash-effect' ), 'trash-effect-active' );
+    }, false );
